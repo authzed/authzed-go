17e9bb3d3b2439ad2d4d2e880d96e15ed88c2c61
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2021-04-29 18:22:33 -0400

    x/pool: init
    Signed-off-by: Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>

---
+++ x/pool/pool_example_test.go
@@ -0,0 +1,45 @@
+package pool_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/authzed/authzed-go"
+	api "github.com/authzed/authzed-go/arrakisapi/api"
+	"github.com/authzed/authzed-go/x/pool"
+)
+
+func ExampleNewClientPool() {
+	clientPool, err := pool.NewClientPool(
+		"grpc.authzed.com:443",
+		10,
+		authzed.Token("my_token_deadbeefdeadbeefdeadbeef"),
+		authzed.SystemCerts(authzed.VerifyCA),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	client, err := clientPool.Get(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close() // Returns client to pool; does not disconnect client.
+
+	client.Check(ctx, &api.CheckRequest{
+		TestUserset: &api.ObjectAndRelation{
+			Namespace: "mytenant/document",
+			ObjectId:  "readme",
+			Relation:  "viewer",
+		},
+		User: &api.User{UserOneof: &api.User_Userset{
+			Userset: &api.ObjectAndRelation{
+				Namespace: "mytenant/user",
+				ObjectId:  "jimmy",
+				Relation:  "...",
+			},
+		}},
+	})
+}
