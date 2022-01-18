9403927575d824e56ccbbf947f29fccd56429ecb
Evan Cordell <cordell.evan@gmail.com>
2022-01-18 09:26:41 -0500

    add middleware for storing and retrieving prefixes from grpc MD

---
+++ pkg/prefix/prefix.go
@@ -0,0 +1,84 @@
+package prefix
+
+import (
+	"context"
+	"github.com/grpc-ecosystem/go-grpc-middleware/util/metautils"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+type ctxKeyType string
+
+// ctxPrefixKey is for storing/fetching from a context
+var ctxPrefixKey ctxKeyType = "prefix"
+
+// grpcMDPrefixKey is for storing/fetching from grpc MD (must be a string)
+const grpcMDPrefixKey = "spicedb-prefix"
+
+// WithPrefixUnaryClientInterceptor annotates requests with the desired prefix
+func WithPrefixUnaryClientInterceptor(prefix string) grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, callOpts ...grpc.CallOption,
+	) error {
+		ctx = metadata.AppendToOutgoingContext(ctx, grpcMDPrefixKey, prefix)
+		return invoker(ctx, method, req, reply, cc, callOpts...)
+	}
+}
+
+// WithPrefixStreamClientInterceptor annotates requests with the desired prefix
+func WithPrefixStreamClientInterceptor(prefix string) grpc.StreamClientInterceptor {
+	return func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+		ctx = metadata.AppendToOutgoingContext(ctx, grpcMDPrefixKey, prefix)
+		return streamer(ctx, desc, cc, method, opts...)
+	}
+}
+
+// PrefixFromContext reads the prefix from the context
+func PrefixFromContext(ctx context.Context) string {
+	if c := ctx.Value(ctxPrefixKey); c != nil {
+		return c.(string)
+	}
+	return ""
+}
+
+// MustPrefixFromContext reads the prefix from the context and panics if it has
+// not been set on the context.
+func MustPrefixFromContext(ctx context.Context) string {
+	prefix := PrefixFromContext(ctx)
+	if prefix == "" {
+		panic("prefix missing")
+	}
+	return prefix
+}
+
+// UnaryServerInterceptor returns a new unary server interceptor that extracts
+// the prefix from the request metadata and adds it to the context
+func UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	prefix := metautils.ExtractIncoming(ctx).Get(grpcMDPrefixKey)
+	return handler(context.WithValue(ctx, ctxPrefixKey, prefix), req)
+}
+
+// StreamServerInterceptor returns a new stream server interceptor that extracts
+// the prefix from the request metadata and adds it to the context
+func StreamServerInterceptor(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	wrapper := &recvWrapper{stream, stream.Context(), stream.Context()}
+	return handler(srv, wrapper)
+}
+
+type recvWrapper struct {
+	grpc.ServerStream
+	initialCtx context.Context
+	ctx        context.Context
+}
+
+func (s *recvWrapper) Context() context.Context {
+	return s.ctx
+}
+
+func (s *recvWrapper) RecvMsg(m interface{}) error {
+	if err := s.ServerStream.RecvMsg(m); err != nil {
+		return err
+	}
+	prefix := metautils.ExtractIncoming(s.initialCtx).Get(grpcMDPrefixKey)
+	s.ctx = context.WithValue(s.initialCtx, ctxPrefixKey, prefix)
+	return nil
+}
