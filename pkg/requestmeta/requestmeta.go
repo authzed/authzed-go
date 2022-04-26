4690599354559e4a426dc0b013f87971861c2e77
Joseph Schorr <josephschorr@users.noreply.github.com>
2022-04-26 15:46:18 -0400

    Add headers for requesting and receiving the server version and other debug information

---
+++ pkg/requestmeta/requestmeta.go
@@ -0,0 +1,39 @@
+package requestmeta
+
+import (
+	"context"
+
+	"google.golang.org/grpc/metadata"
+)
+
+// RequestMetadataHeaderKey defines a key in the request metadata header.
+type RequestMetadataHeaderKey string
+
+// BoolRequestMetadataHeaderKey defines a key for a boolean vlaue in the request metadata header.
+type BoolRequestMetadataHeaderKey RequestMetadataHeaderKey
+
+const (
+	// RequestServerVersion, if specified in a request header, asks SpiceDB to return its
+	// server version in the response header (if supported).
+	// Value: `1`
+	RequestServerVersion BoolRequestMetadataHeaderKey = "io.spicedb.requestversion"
+)
+
+// AddRequestHeaders returns a new context with the given values as request headers.
+func AddRequestHeaders(ctx context.Context, keys ...BoolRequestMetadataHeaderKey) context.Context {
+	values := make(map[RequestMetadataHeaderKey]string, len(keys))
+	for _, key := range keys {
+		values[RequestMetadataHeaderKey(key)] = "1"
+	}
+	return SetRequestHeaders(ctx, values)
+}
+
+// SetRequestHeaders returns a new context with the given values as request headers.
+func SetRequestHeaders(ctx context.Context, values map[RequestMetadataHeaderKey]string) context.Context {
+	pairs := make([]string, 0, len(values)*2)
+	for key, value := range values {
+		pairs = append(pairs, string(key))
+		pairs = append(pairs, value)
+	}
+	return metadata.NewOutgoingContext(ctx, metadata.Pairs(pairs...))
+}
