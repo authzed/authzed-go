6d24942e0dd4620cf3f566213ff12a728cda06c4
Joseph Schorr <josephschorr@users.noreply.github.com>
2025-02-17 15:34:46 -0500

    Update authzed-go to promote reflection APIs

--- proto/authzed/api/v1/schema_service_grpc.pb.go
+++ proto/authzed/api/v1/schema_service_grpc.pb.go
@@ -19,8 +19,12 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	SchemaService_ReadSchema_FullMethodName  = "/authzed.api.v1.SchemaService/ReadSchema"
-	SchemaService_WriteSchema_FullMethodName = "/authzed.api.v1.SchemaService/WriteSchema"
+	SchemaService_ReadSchema_FullMethodName            = "/authzed.api.v1.SchemaService/ReadSchema"
+	SchemaService_WriteSchema_FullMethodName           = "/authzed.api.v1.SchemaService/WriteSchema"
+	SchemaService_ReflectSchema_FullMethodName         = "/authzed.api.v1.SchemaService/ReflectSchema"
+	SchemaService_ComputablePermissions_FullMethodName = "/authzed.api.v1.SchemaService/ComputablePermissions"
+	SchemaService_DependentRelations_FullMethodName    = "/authzed.api.v1.SchemaService/DependentRelations"
+	SchemaService_DiffSchema_FullMethodName            = "/authzed.api.v1.SchemaService/DiffSchema"
 )
 
 // SchemaServiceClient is the client API for SchemaService service.
@@ -37,6 +41,21 @@ type SchemaServiceClient interface {
 	ReadSchema(ctx context.Context, in *ReadSchemaRequest, opts ...grpc.CallOption) (*ReadSchemaResponse, error)
 	// Write overwrites the current Object Definitions for a Permissions System.
 	WriteSchema(ctx context.Context, in *WriteSchemaRequest, opts ...grpc.CallOption) (*WriteSchemaResponse, error)
+	// ReflectSchema reflects the current schema stored in SpiceDB, returning a structural
+	// form of the schema for use by client tooling.
+	ReflectSchema(ctx context.Context, in *ReflectSchemaRequest, opts ...grpc.CallOption) (*ReflectSchemaResponse, error)
+	// ComputablePermissions returns the set of permissions that compute based off a relation
+	// in the current schema. For example, if the schema has a relation `viewer` and a permission
+	// `view` defined as `permission view = viewer + editor`, then the
+	// computable permissions for the relation `viewer` will include `view`.
+	ComputablePermissions(ctx context.Context, in *ComputablePermissionsRequest, opts ...grpc.CallOption) (*ComputablePermissionsResponse, error)
+	// DependentRelations returns the set of relations and permissions that used
+	// to compute a permission, recursively, in the current schema. It is the
+	// inverse of the ComputablePermissions API.
+	DependentRelations(ctx context.Context, in *DependentRelationsRequest, opts ...grpc.CallOption) (*DependentRelationsResponse, error)
+	// DiffSchema returns the difference between the specified schema and the current
+	// schema stored in SpiceDB.
+	DiffSchema(ctx context.Context, in *DiffSchemaRequest, opts ...grpc.CallOption) (*DiffSchemaResponse, error)
 }
 
 type schemaServiceClient struct {
@@ -67,6 +86,46 @@ func (c *schemaServiceClient) WriteSchema(ctx context.Context, in *WriteSchemaRe
 	return out, nil
 }
 
+func (c *schemaServiceClient) ReflectSchema(ctx context.Context, in *ReflectSchemaRequest, opts ...grpc.CallOption) (*ReflectSchemaResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(ReflectSchemaResponse)
+	err := c.cc.Invoke(ctx, SchemaService_ReflectSchema_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *schemaServiceClient) ComputablePermissions(ctx context.Context, in *ComputablePermissionsRequest, opts ...grpc.CallOption) (*ComputablePermissionsResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(ComputablePermissionsResponse)
+	err := c.cc.Invoke(ctx, SchemaService_ComputablePermissions_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *schemaServiceClient) DependentRelations(ctx context.Context, in *DependentRelationsRequest, opts ...grpc.CallOption) (*DependentRelationsResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(DependentRelationsResponse)
+	err := c.cc.Invoke(ctx, SchemaService_DependentRelations_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *schemaServiceClient) DiffSchema(ctx context.Context, in *DiffSchemaRequest, opts ...grpc.CallOption) (*DiffSchemaResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(DiffSchemaResponse)
+	err := c.cc.Invoke(ctx, SchemaService_DiffSchema_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // SchemaServiceServer is the server API for SchemaService service.
 // All implementations must embed UnimplementedSchemaServiceServer
 // for forward compatibility.
@@ -81,6 +140,21 @@ type SchemaServiceServer interface {
 	ReadSchema(context.Context, *ReadSchemaRequest) (*ReadSchemaResponse, error)
 	// Write overwrites the current Object Definitions for a Permissions System.
 	WriteSchema(context.Context, *WriteSchemaRequest) (*WriteSchemaResponse, error)
+	// ReflectSchema reflects the current schema stored in SpiceDB, returning a structural
+	// form of the schema for use by client tooling.
+	ReflectSchema(context.Context, *ReflectSchemaRequest) (*ReflectSchemaResponse, error)
+	// ComputablePermissions returns the set of permissions that compute based off a relation
+	// in the current schema. For example, if the schema has a relation `viewer` and a permission
+	// `view` defined as `permission view = viewer + editor`, then the
+	// computable permissions for the relation `viewer` will include `view`.
+	ComputablePermissions(context.Context, *ComputablePermissionsRequest) (*ComputablePermissionsResponse, error)
+	// DependentRelations returns the set of relations and permissions that used
+	// to compute a permission, recursively, in the current schema. It is the
+	// inverse of the ComputablePermissions API.
+	DependentRelations(context.Context, *DependentRelationsRequest) (*DependentRelationsResponse, error)
+	// DiffSchema returns the difference between the specified schema and the current
+	// schema stored in SpiceDB.
+	DiffSchema(context.Context, *DiffSchemaRequest) (*DiffSchemaResponse, error)
 	mustEmbedUnimplementedSchemaServiceServer()
 }
 
@@ -96,6 +170,18 @@ func (UnimplementedSchemaServiceServer) ReadSchema(context.Context, *ReadSchemaR
 }
 func (UnimplementedSchemaServiceServer) WriteSchema(context.Context, *WriteSchemaRequest) (*WriteSchemaResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method WriteSchema not implemented")
+}
+func (UnimplementedSchemaServiceServer) ReflectSchema(context.Context, *ReflectSchemaRequest) (*ReflectSchemaResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ReflectSchema not implemented")
+}
+func (UnimplementedSchemaServiceServer) ComputablePermissions(context.Context, *ComputablePermissionsRequest) (*ComputablePermissionsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ComputablePermissions not implemented")
+}
+func (UnimplementedSchemaServiceServer) DependentRelations(context.Context, *DependentRelationsRequest) (*DependentRelationsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DependentRelations not implemented")
+}
+func (UnimplementedSchemaServiceServer) DiffSchema(context.Context, *DiffSchemaRequest) (*DiffSchemaResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DiffSchema not implemented")
 }
 func (UnimplementedSchemaServiceServer) mustEmbedUnimplementedSchemaServiceServer() {}
 func (UnimplementedSchemaServiceServer) testEmbeddedByValue()                       {}
@@ -154,6 +240,78 @@ func _SchemaService_WriteSchema_Handler(srv interface{}, ctx context.Context, de
 	return interceptor(ctx, in, info, handler)
 }
 
+func _SchemaService_ReflectSchema_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ReflectSchemaRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SchemaServiceServer).ReflectSchema(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: SchemaService_ReflectSchema_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SchemaServiceServer).ReflectSchema(ctx, req.(*ReflectSchemaRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _SchemaService_ComputablePermissions_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ComputablePermissionsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SchemaServiceServer).ComputablePermissions(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: SchemaService_ComputablePermissions_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SchemaServiceServer).ComputablePermissions(ctx, req.(*ComputablePermissionsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _SchemaService_DependentRelations_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DependentRelationsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SchemaServiceServer).DependentRelations(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: SchemaService_DependentRelations_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SchemaServiceServer).DependentRelations(ctx, req.(*DependentRelationsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _SchemaService_DiffSchema_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DiffSchemaRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SchemaServiceServer).DiffSchema(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: SchemaService_DiffSchema_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SchemaServiceServer).DiffSchema(ctx, req.(*DiffSchemaRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // SchemaService_ServiceDesc is the grpc.ServiceDesc for SchemaService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -168,6 +326,22 @@ var SchemaService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "WriteSchema",
 			Handler:    _SchemaService_WriteSchema_Handler,
+		},
+		{
+			MethodName: "ReflectSchema",
+			Handler:    _SchemaService_ReflectSchema_Handler,
+		},
+		{
+			MethodName: "ComputablePermissions",
+			Handler:    _SchemaService_ComputablePermissions_Handler,
+		},
+		{
+			MethodName: "DependentRelations",
+			Handler:    _SchemaService_DependentRelations_Handler,
+		},
+		{
+			MethodName: "DiffSchema",
+			Handler:    _SchemaService_DiffSchema_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
